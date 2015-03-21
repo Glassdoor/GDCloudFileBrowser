9d5700a7992bd5c72902304f66ba6265c22ba000
Linto Mathew <lintolal@gmail.com>
2015-03-21 07:34:04 -0700

    Added 'LiveSDK' and MBProgressHUB as pod projects

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// LiveSDK
+#define COCOAPODS_POD_AVAILABLE_LiveSDK
+#define COCOAPODS_VERSION_MAJOR_LiveSDK 5
+#define COCOAPODS_VERSION_MINOR_LiveSDK 6
+#define COCOAPODS_VERSION_PATCH_LiveSDK 1
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 1
+
