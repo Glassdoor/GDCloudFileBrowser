53f5a7dfe82b4cf1cda7dfc29c60929083d7d708
Linto Mathew <linto.mathew@glassdoor.com>
2015-05-21 16:39:39 -0700

    Added some documentation for KeysHelper

--- GDCloudFileBrowser/Core/KeysHelper.h
+++ GDCloudFileBrowser/Core/KeysHelper.h
@@ -10,12 +10,34 @@
 
 @interface KeysHelper : NSObject
 
+/**
+ @returns The KeysHelper singleton object
+ */
 + (KeysHelper *)sharedHelper;
 
+/**
+ @returns The dropboxClientId from Keys.plist
+ */
 - (NSString *)dropboxClientId;
+
+/**
+ @returns The dropboxClientSecret from Keys.plist
+ */
 - (NSString *)dropboxClientSecret;
+
+/**
+ @returns The googleDriveClientId from Keys.plist
+ */
 - (NSString *)googleDriveClientId;
+
+/**
+ @returns The googleDriveClientSecret from Keys.plist
+ */
 - (NSString *)googleDriveClientSecret;
+
+/**
+ @returns The googleDriveClientKeychainName from Keys.plist
+ */
 - (NSString *)googleDriveClientKeychainName;
 
 @end
